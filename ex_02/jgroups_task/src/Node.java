c987270a25300063c22a8cfa412431f0a1a2825e
nwroblewski <nwroblewski@pop-os.localdomain>
2019-03-28 16:05:00 +0100

    Main class Node implemented.

--- ex_02/jgroups_task/src/Node.java
+++ ex_02/jgroups_task/src/Node.java
@@ -1,39 +1,62 @@
 import org.jgroups.JChannel;
-import org.jgroups.Message;
 import org.jgroups.ReceiverAdapter;
-import org.jgroups.View;
-import org.jgroups.util.Util;
 
-import java.io.DataOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.HashMap;
-import java.util.Locale;
-import java.util.concurrent.TimeUnit;
+import java.util.Scanner;
+import java.util.logging.LogManager;
 
 public class Node extends ReceiverAdapter{
 
-    //TODO przydalby sie dekorator dla wyswietlania stanu hashmapy, klikania sobie ladnie w get/put itd
 
-    private DistributedMap DMap = new DistributedMap(new HashMap<>());
-    JChannel channel;
-
-    private void start() throws Exception{
-
-        channel = new JChannel();
-        channel.setReceiver(this);
-        channel.connect("TEST_CLUSTER");
-        eventLoop();
-        channel.close();
-    }
-
-    private void eventLoop(){
-
-    }
     public static void main(String[] args) throws Exception{
-        System.setProperty("java.net.preferIPv4Stack","true");
-        new Node().start();
+
+        LogManager.getLogManager().reset();
+        DistributedMap map = new DistributedMap();
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            try{
+                String op = scanner.next();
+                String key;
+                Integer value;
+
+                switch(op.toUpperCase()){
+                    case "PUT":
+                        System.out.println("provide key");
+                        key = scanner.next();
+                        System.out.println("provide value");
+                        value = scanner.nextInt();
+                        map.put(key,value);
+                        break;
+                    case "REMOVE":
+                        System.out.println("provide key");
+                        key = scanner.next();
+                        System.out.println(map.remove(key));
+                        break;
+                    case "GET":
+                        System.out.println("provide key");
+                        key = scanner.next();
+                        System.out.println(map.get(key));
+                        break;
+                    case "PRINT":
+                        System.out.println(map);
+                        break;
+                    case "ISIN":
+                        System.out.println("provide key");
+                        key = scanner.next();
+                        System.out.println(map.containsKey(key));
+                        break;
+                    case "EXIT":
+                        map.close();
+                        return;
+                        default:
+                            System.out.println("Invalid operation. Valid operations are:" +
+                                    "put, remove, get, print, isin, exit");
+
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+        }
     }
 }
